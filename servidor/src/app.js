73798cfb9441de0ae44a9b25c560a14a06c325e3
rogertavaress <rogerioctf@gmail.com>
2019-09-11 10:45:06 -0300

    chore: Alteraçao nas permissoes de acesso

--- servidor/src/app.js
+++ servidor/src/app.js
@@ -14,7 +14,7 @@ class App {
     this.server.use(express.json());
     this.server.use(express.static("public"));
     this.server.use((req, res, next) => {
-      res.header("Access-Control-Allow-Origin", "http://localhost:8081");
+      res.header("Access-Control-Allow-Origin", "*");
       res.header(
         "Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept"
